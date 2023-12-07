01b31637ee9db0d0ce156e89f8ecd5b8adbd9dc8
knir4 <knir4@gmail.com>
2023-12-07 15:51:48 +0200

    final project

--- backend/utils/paypal.js
+++ backend/utils/paypal.js
@@ -72,6 +72,7 @@ export async function checkIfNewTransaction(orderModel, paypalTransactionId) {
  */
 export async function verifyPayPalPayment(paypalTransactionId) {
   const accessToken = await getPayPalAccessToken();
+  console.log('nir ', accessToken);
   const paypalResponse = await fetch(
     `${PAYPAL_API_URL}/v2/checkout/orders/${paypalTransactionId}`,
     {
